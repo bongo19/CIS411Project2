7fdd0176bc8b995a380a2df50140f9cb91d410a6
AB244 <ajbenn04@louisville.edu>
2014-04-23 15:41:21 -0400

    Working

--- MeetUs/MeetUs/Migrations/Configuration.cs
+++ MeetUs/MeetUs/Migrations/Configuration.cs
@@ -1,62 +1,61 @@
-using System.Web.Security;
-using WebMatrix.WebData;
-
 namespace MeetUs.Migrations
 {
     using System;
-    using System.Data.Entity;
-    using System.Data.Entity.Migrations;
-    using System.Linq;
+using System.Data.Entity;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Web.Security;
+using WebMatrix.WebData;
 
     internal sealed class Configuration : DbMigrationsConfiguration<MeetUs.Context.DataContext>
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = false;
+            AutomaticMigrationsEnabled = true;
         }
 
         protected override void Seed(MeetUs.Context.DataContext context)
         {
+WebSecurity.InitializeDatabaseConnection(
+               "DefaultConnection",
+               "UserProfile",
+               "UserId",
+               "UserName", autoCreateTables: true);
 
-            WebSecurity.InitializeDatabaseConnection(
-                "DefaultConnection",
-                "UserProfile",
-                "UserId",
-                "UserName", autoCreateTables: true);
+           if(!Roles.RoleExists("Administrator"))
+               Roles.CreateRole("Administrator");
 
-            if(!Roles.RoleExists("Administrator"))
-                Roles.CreateRole("Administrator");
+           if (!WebSecurity.UserExists("channing"))
+               WebSecurity.CreateUserAndAccount(
+                   "channing",
+                   "password"
+                   );
 
-            if (!WebSecurity.UserExists("channing"))
-                WebSecurity.CreateUserAndAccount(
-                    "channing",
-                    "password"
-                    );
+           if (!WebSecurity.UserExists("guest"))
+               WebSecurity.CreateUserAndAccount(
+                   "guest",
+                   "password"
+                   );
 
-            if (!WebSecurity.UserExists("guest"))
-                WebSecurity.CreateUserAndAccount(
-                    "guest",
-                    "password"
-                    );
-
-            if(!Roles.GetRolesForUser("channing").Contains("Administrator"))
-                Roles.AddUsersToRoles(new [] {"channing"}, new [] {"Administrator"});
-
+           if(!Roles.GetRolesForUser("channing").Contains("Administrator"))
+               Roles.AddUsersToRoles(new [] {"channing"}, new [] {"Administrator"});
 
 
 
-            //  This method will be called after migrating to the latest version.
 
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
+           //  This method will be called after migrating to the latest version.
+
+           //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+           //  to avoid creating duplicate seed data. E.g.
+           //
+           //    context.People.AddOrUpdate(
+           //      p => p.FullName,
+           //      new Person { FullName = "Andrew Peters" },
+           //      new Person { FullName = "Brice Lambson" },
+           //      new Person { FullName = "Rowan Miller" }
+           //    );
+           //
+       
         }
     }
 }
